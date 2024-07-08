c1a40488d079edeef2d8e2f16ff75b3b7027b95d
TimHedden <160011202+TimHedden@users.noreply.github.com>
2024-07-08 12:54:45 +0200

    Delete Project2 directory

--- Project2/assets/resizing.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('resize', function() {
-    Plotly.Plots.resize(document.getElementById('scatter-plot'));
-    Plotly.Plots.resize(document.getElementById('nutrition-correlation-heatmap'));
-    Plotly.Plots.resize(document.getElementById('boxplot-1'));
-    Plotly.Plots.resize(document.getElementById('boxplot-2'));
-    Plotly.Plots.resize(document.getElementById('heatmap'));
-});
